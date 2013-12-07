3e944c016ed0798294e926a7bd429082be0634aa
Tyler Wasden <twasden@gmail.com>
2013-12-07 15:40:22 -0600

    Thanks to Fremen we got the google static map working on the show page

--- app/models/showtimes.rb
+++ app/models/showtimes.rb
@@ -3,15 +3,19 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(open("http://www.google.com/movies?hl=en&near=Austin,+TX&ei=JeSiUsuHDPOO2gWUy4CgCg&q=hunger+gams")) 
 puts doc.class
+@ad1 = doc.css(".address")[0].text
+@ad2 = doc.css(".address")[1].text
+@ad3 = doc.css(".address")[2].text
 puts
 puts doc.css(".name")[0].text
-puts doc.css(".address")[0].text
+puts @ad1
 puts doc.css(".times")[0].text
 puts 
 puts doc.css(".name")[1].text
-puts doc.css(".address")[1].text
+puts @ad2
 puts doc.css(".times")[1].text
 puts 
 puts doc.css(".name")[2].text
-puts doc.css(".address")[2].text
+puts @ad3
 puts doc.css(".times")[2].text
+
