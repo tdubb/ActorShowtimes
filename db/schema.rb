004efcd41c9992275b5114f7227f6c9f8bd255ba
Qualquer Valor <qualquervalor@gmail.com>
2013-12-08 01:28:51 -0600

    migrate user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20131208044022) do
 
   create_table "actors", force: true do |t|
     t.string   "name"
