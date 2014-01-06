5fff8391109da36f575bc2dbc1f58ce6e7ce9c91
Qualquer Valor <qualquervalor@gmail.com>
2014-01-05 22:29:46 -0600

    creating actor controller rspec tests

---
+++ spec/controllers/actors_controller_spec.rb
@@ -0,0 +1,31 @@
+#spec/controllers/actors_controller_apec.rb
+require 'spec_helper'
+
+describe ActorsController do
+
+  describe "GET index" do
+    # it "assigns @teams" do
+    #   team = Team.create
+    #   get :index
+    #   expect(assigns(:teams)).to eq([team])
+    # end
+
+    # it "renders the index template" do
+    #   get :index
+    #   expect(response).to render_template("index")
+    # end
+
+    it "should get show" do
+      get :show
+      assert_response :success
+    end
+    
+  end
+
+end
+
+# new
+# create
+# index
+# show
+# destroy
