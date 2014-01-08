7d634608cc4382d97d6ba29f5e2ac08150e1d4b1
Tyler Wasden <twasden@gmail.com>
2014-01-08 09:57:55 -0600

    changed ip lookup from remote to local to make more accurate

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -140,7 +140,7 @@ class ActorsController < ApplicationController
      @address_array =[]
      @title_array =[]
     @zipcode = params[:zipcode]
-    ip = request.remote_ip 
+    ip = request.location.ip 
     obj = Geocoder.search(ip)
     if !@zipcode.present? || @zipcode.length < 5 
       @zipcode = obj[0].postal_code
