408e440237e43151f4bc5ad7adc6a164933bc7eb
Qualquer Valor <qualquervalor@gmail.com>
2014-01-09 11:58:29 -0600

    reverted geocoder ip to request.remote_ip

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -140,7 +140,7 @@ class ActorsController < ApplicationController
      @address_array =[]
      @title_array =[]
     @zipcode = params[:zipcode]
-    ip = request.location.ip 
+    ip = request.remote_ip 
     obj = Geocoder.search(ip)
     if !@zipcode.present? || @zipcode.length < 5 
       @zipcode = obj[0].postal_code
